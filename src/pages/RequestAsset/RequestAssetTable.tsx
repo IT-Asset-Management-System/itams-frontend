eb8dc570a7de54c9ca3eee393c8f4d1e345e686f
luongbs01 <54256832+luongbs01@users.noreply.github.com>
2023-03-12 22:19:32 +0700

    Fix: row per page

--- src/pages/RequestAsset/RequestAssetTable.tsx
+++ src/pages/RequestAsset/RequestAssetTable.tsx
@@ -248,7 +248,9 @@ export default function RequestAssetTable() {
   const [loading, setLoading] = React.useState<boolean>(false);
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(
-    Number(getPref(Prefs.ROWS_PER_PAGE)) ?? 5,
+    Number(getPref(Prefs.ROWS_PER_PAGE)) === 0
+      ? 5
+      : Number(getPref(Prefs.ROWS_PER_PAGE)),
   );
   const [rows, setRows] = React.useState<RequestAsset[]>([]);
 
